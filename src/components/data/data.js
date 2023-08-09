073b407368e3818a0cd2598ca6314880d34a126d
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-09 20:46:46 +0200

    added more mock data

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,6 +1,38 @@
 export const data = [
   {
     id: 1,
+    name: "Hamburger",
+    category: "burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80",
+    price: "$",
+  },
+  {
+    id: 2,
+    name: "Pesto Pasta",
+    category: "pasta",
+    image:
+      "https://images.unsplash.com/photo-1605590955562-be1a5fda4161?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1398&q=80",
+    price: "$",
+  },
+  {
+    id: 3,
+    name: "Pizza proscuito rucola",
+    category: "pizza",
+    image:
+      "https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    price: "$",
+  },
+  {
+    id: 4,
+    name: "Insalata verde",
+    category: "salad",
+    image:
+      "https://images.unsplash.com/photo-1608032077018-c9aad9565d29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    price: "$",
+  },
+  {
+    id: 5,
     name: "Double Cheesburger",
     category: "burger",
     image:
@@ -8,7 +40,7 @@ export const data = [
     price: "$$",
   },
   {
-    id: 2,
+    id: 6,
     name: "Pepperoni Pizza",
     category: "pizza",
     image:
@@ -16,7 +48,7 @@ export const data = [
     price: "$$",
   },
   {
-    id: 3,
+    id: 7,
     name: "Pasta Bolognese",
     category: "pasta",
     image:
@@ -24,7 +56,7 @@ export const data = [
     price: "$$",
   },
   {
-    id: 4,
+    id: 8,
     name: "Caesar Salad",
     category: "salad",
     image:
@@ -32,7 +64,7 @@ export const data = [
     price: "$$",
   },
   {
-    id: 5,
+    id: 9,
     name: "Pulled Pork Burger",
     category: "burger",
     image:
@@ -40,7 +72,7 @@ export const data = [
     price: "$$$",
   },
   {
-    id: 6,
+    id: 10,
     name: "Pasta Aglio E Olio",
     category: "pasta",
     image:
@@ -48,7 +80,7 @@ export const data = [
     price: "$$$",
   },
   {
-    id: 7,
+    id: 11,
     name: "Salmon Salad",
     category: "salad",
     image:
@@ -56,11 +88,43 @@ export const data = [
     price: "$$$",
   },
   {
-    id: 8,
+    id: 12,
     name: "Pizza Margherita",
     category: "pizza",
     image:
       "https://images.unsplash.com/photo-1564936281291-294551497d81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1352&q=80",
     price: "$$$",
+  },
+  {
+    id: 13,
+    name: "Truffel Burger",
+    category: "burger",
+    image:
+      "https://images.unsplash.com/photo-1549611016-3a70d82b5040?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1430&q=80",
+    price: "$$$$",
+  },
+  {
+    id: 14,
+    name: "Pasta al Tartufo",
+    category: "pasta",
+    image:
+      "https://images.unsplash.com/photo-1516685018646-549198525c1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    price: "$$$$",
+  },
+  {
+    id: 15,
+    name: "Salad Fruits Des Mer",
+    category: "salad",
+    image:
+      "https://images.unsplash.com/photo-1551248429-40975aa4de74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1290&q=80",
+    price: "$$$$",
+  },
+  {
+    id: 16,
+    name: "Pizza al Trufo",
+    category: "salad",
+    image:
+      "https://images.unsplash.com/photo-1586934729750-2e32c19c2320?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    price: "$$$$",
   }
 ];
