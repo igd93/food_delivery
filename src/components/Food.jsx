05824f983f7f72c652c47c34aed51d91ab720df5
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-10 19:49:13 +0200

    now all the foods are properly diplayed and styled

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { data } from "./data/data.js";
+import { useState } from "react";
 
 const Food = () => {
-  console.log(data);
+  const [foods, setFoods] = useState(data);
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
@@ -79,8 +80,28 @@ const Food = () => {
         </div>
       </div>
       {/*Display foods */}
-      <div>
-
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border shadow-lg rounded-lg 
+            hover:scale-110 duration-300"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-lg"
+            />
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white p-1 rounded-full">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
